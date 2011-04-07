352c11f450242977ac28a316795aa1e5b4694422
tonyskapunk <tonysk8@gmx.net>
2011-04-07 01:21:55 +0000

    Remove chomp as facter complains about it.

--- manifests/modules/load/lib/facter/load.rb
+++ manifests/modules/load/lib/facter/load.rb
@@ -1,11 +1,10 @@
 require 'facter'
 sysloads = %x{/usr/bin/uptime|awk '{print $10 $11 $12}'}.split(",")
 factnames = [ "load1", "load5", "load15" ]
-
 factnames.each do |factname|
   Facter.add(factname) do
     setcode do
-      sysloads.pop.chomp
+      sysloads.pop
     end
   end
 end
