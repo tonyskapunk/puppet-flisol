7549553383245dff7adba176e07780f19860ffbf
tonyskapunk <tonysk8@gmx.net>
2011-04-09 03:14:14 +0000

    Add content to web class and implement flisol class.

--- manifests/classes/web.pp
+++ manifests/classes/web.pp
@@ -1,2 +1,27 @@
-class web {
+class web($sitename) {
+  $apache = $operatingsystem ? {
+    /(CentOS|RedHat|Fedora)/ => "httpd",
+    /(Debian|Ubuntu)/        => "apache2"
+  }
+  file {
+    "/etc/$apache/conf.d/$sitename.conf":
+      owner   => root,
+      group   => root,
+      mode    => 644,
+      source  => "puppet:///modules/http/$sitename.conf",
+      notify  => Service["$apache"],
+      require => Package["$apache"];
+    "/opt/www":
+      ensure => directory,
+      mode   => 755;
+    "/opt/www/$sitename":
+      ensure => directory,
+      mode   => 755;
+    "/opt/www/$sitename/index.html":
+      source => template("http/$sitename.erb");
+  }
+}
+
+class flisol {
+  class { "web": sitename => "flisol"; }
 }
